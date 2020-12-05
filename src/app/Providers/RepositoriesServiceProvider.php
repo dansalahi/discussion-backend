7a21e7ae3d6fcea82b64b05234ed5ff7ce7ca692
danial <dev.salahi@gmail.com>
2020-12-05 15:49:04 +0330

    added channed repos to RepositoriesServiceProvider.php

--- src/app/Providers/RepositoriesServiceProvider.php
+++ src/app/Providers/RepositoriesServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\ChannelRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\EloquentChannelRepository;
 use App\Repositories\EloquentUserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,7 +12,8 @@ class RepositoriesServiceProvider extends ServiceProvider
 {
 
     public $bindings = [
-        UserRepositoryInterface::class => EloquentUserRepository::class,
+        UserRepositoryInterface::class    => EloquentUserRepository::class,
+        ChannelRepositoryInterface::class => EloquentChannelRepository::class,
     ];
 
     /**
