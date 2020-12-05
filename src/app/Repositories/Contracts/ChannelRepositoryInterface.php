a2ac56a33822e0d13f8deb728c6c363bc132053f
danial <dev.salahi@gmail.com>
2020-12-05 15:47:36 +0330

    added Channel repositories

---
+++ src/app/Repositories/Contracts/ChannelRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Repositories\Contracts;
+
+
+use Illuminate\Support\Collection;
+
+interface ChannelRepositoryInterface
+{
+    public function getModel();
+
+    public function all(): Collection;
+
+    public function findById(int $id);
+
+    public function create(array $data);
+
+    public function update(int $id, array $data);
+}
---
+++ src/app/Repositories/EloquentChannelRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Channel;
+use App\Repositories\Contracts\ChannelRepositoryInterface;
+use Illuminate\Support\Collection;
+
+class EloquentChannelRepository implements ChannelRepositoryInterface
+{
+
+    public function getModel()
+    {
+        return Channel::query();
+    }
+
+    public function all(): Collection
+    {
+        return $this->getModel()->latest()->get();
+    }
+
+    public function findById(int $id)
+    {
+        return $this->getModel()->find($id);
+    }
+
+    public function create($data)
+    {
+        return $this->getModel()->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        return $this->getModel()->find($id)->update($data);
+    }
+}
