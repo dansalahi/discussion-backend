573a48420d73f1319f14b39503fcfcd9bc998ef9
danial <dev.salahi@gmail.com>
2021-02-27 23:57:55 +0330

    implemented the roles and permissions seeder

---
+++ src/database/seeds/RoleAndPermissionSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (\Spatie\Permission\Models\Role::whereName(config('permissions.default_roles')[0])->count() < 1) {
+            // Creating roles bases on the permission's config
+            foreach (config('permissions.default_roles') as $roleName) {
+                \Spatie\Permission\Models\Role::create([
+                    'name' => $roleName
+                ]);
+            }
+
+        }
+
+        if(\Spatie\Permission\Models\Permission::whereName(config('permissions.default_permissions')[0])->count() < 1) {
+            // Creating permissions bases on the permission's config
+            foreach (config('permissions.default_permissions') as $permissionName) {
+                \Spatie\Permission\Models\Permission::create([
+                    'name' => $permissionName
+                ]);
+            }
+        }
+
+    }
+}
