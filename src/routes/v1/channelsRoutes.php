747f369b7a0b31da02d36eabf237b4b9c6da39b1
danial <dev.salahi@gmail.com>
2021-02-28 19:02:53 +0330

    separated channels/v1's api

---
+++ src/routes/v1/channelsRoutes.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\Channels\ChannelsController;
+
+// Channel Routes
+Route::namespace('Channels')->prefix('/channel')->group(function () {
+    Route::get('/all', [ChannelsController::class, 'getAllChannels'])->name('channels.all');
+    Route::post('/store', [ChannelsController::class, 'store'])->name('channel.store');
+    Route::put('/update', [ChannelsController::class, 'update'])->name('channel.update');
+    Route::delete('/destroy', [ChannelsController::class, 'destroy'])->name('channel.destroy');
+});
