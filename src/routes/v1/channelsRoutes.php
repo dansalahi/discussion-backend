ffc52cda76ba8c854f05485f07a95e0933e6a84b
danial <dev.salahi@gmail.com>
2021-03-03 18:39:40 +0330

    added sanctum auth middleware

--- src/routes/v1/channelsRoutes.php
+++ src/routes/v1/channelsRoutes.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\v1\Channels\ChannelsController;
 Route::namespace('Channels')->prefix('/channel')->group(function () {
     Route::get('/all', [ChannelsController::class, 'getAllChannels'])->name('channels.all');
 
-    Route::middleware(['can:channel management'])->group(function () {
+    Route::middleware(['can:channel management','auth:sanctum'])->group(function () {
         Route::post('/store', [ChannelsController::class, 'store'])->name('channel.store');
         Route::put('/update', [ChannelsController::class, 'update'])->name('channel.update');
         Route::delete('/destroy', [ChannelsController::class, 'destroy'])->name('channel.destroy');
