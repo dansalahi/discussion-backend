7a3216e1d1a4ed4020098bd6421079c298f0c214
danial <dev.salahi@gmail.com>
2021-03-09 13:43:58 +0330

    binding thread repository and contract in the Repositories Service Provider

--- src/app/Providers/RepositoriesServiceProvider.php
+++ src/app/Providers/RepositoriesServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Repositories\Contracts\ChannelRepositoryInterface;
+use App\Repositories\Contracts\ThreadRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\EloquentChannelRepository;
+use App\Repositories\EloquentThreadRepository;
 use App\Repositories\EloquentUserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,8 +14,9 @@ class RepositoriesServiceProvider extends ServiceProvider
 {
 
     public $bindings = [
-        UserRepositoryInterface::class    => EloquentUserRepository::class,
+        UserRepositoryInterface::class => EloquentUserRepository::class,
         ChannelRepositoryInterface::class => EloquentChannelRepository::class,
+        ThreadRepositoryInterface::class => EloquentThreadRepository::class
     ];
 
     /**
