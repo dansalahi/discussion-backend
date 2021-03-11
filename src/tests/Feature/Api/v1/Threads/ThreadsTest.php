795ce19e607be1424d10069d325a7d877858ff06
danial <dev.salahi@gmail.com>
2021-03-11 11:09:09 +0330

    thread tests for validate request and store method

--- src/tests/Feature/Api/v1/Threads/ThreadsTest.php
+++ src/tests/Feature/Api/v1/Threads/ThreadsTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature\Api\v1\Threads;
 
+use App\Channel;
 use App\Thread;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class ThreadsTest extends TestCase
@@ -27,6 +30,33 @@ class ThreadsTest extends TestCase
         $response = $this->get(route('threads.show', [$thread->slug]));
         $response->assertStatus(Response::HTTP_OK);
 
+    }
+
+    /**
+     * @test a thread request to be validated
+     */
+    public function test_store_a_channel_should_be_validated()
+    {
+        $response = $this->postJson(route('threads.store'), []);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+
+    /**
+     * @test a thread can stored on DB
+     */
+    public function test_a_thread_should_be_stored()
+    {
+
+        Sanctum::actingAs(factory(User::class)->create());
+
+        $response = $this->postJson(route('threads.store'), [
+            'title' => 'React',
+            'content' => 'React is great',
+            'channel_id' => factory(Channel::class)->create()->id
+        ]);
+
+        $response->assertStatus(Response::HTTP_CREATED);
     }
 
 }
